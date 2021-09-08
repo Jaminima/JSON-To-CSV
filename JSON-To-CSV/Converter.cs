1628ad2a6b97f6600dd8f4078a9a13dd6a37b5a6
Oscar Davies <oscar.william.davies@outlook.com>
2021-09-08 09:27:58 +0100

    Support Deep Objects

--- JSON-To-CSV/Converter.cs
+++ JSON-To-CSV/Converter.cs
@@ -10,6 +10,53 @@ namespace JSON_To_CSV
     {
         #region Methods
 
+        private static List<string> GetRow(JObject obj, ref List<string> collumns, string col_name_prefix="")
+        {
+            List<string> row = new List<string>();
+
+            foreach (JProperty token in obj.Children())
+            {
+                string colName = col_name_prefix + token.Name;
+
+                Type token_type = token.Value.GetType();
+
+                if (token_type == typeof(JObject))
+                {
+                    List<string> innerRow = GetRow((JObject)token.Value, ref collumns, token.Name+"-");
+
+                    for (int i = row.Count; i < collumns.Count; i++) row.Add("");
+                    for (int i = 0; i < row.Count; i++)
+                    {
+                        if (innerRow[i] != "") row[i] = innerRow[i];
+                    }
+                }
+                else if (token_type == typeof(JArray))
+                {
+                    Console.WriteLine("Ignored JAarray");
+                }
+                else if (token_type == typeof(JValue))
+                {
+                    if (!collumns.Contains(colName))
+                    {
+                        collumns.Add(colName);
+                    }
+
+                    for (int i = row.Count; i < collumns.Count; i++) row.Add("");
+
+                    int idx = collumns.FindIndex(0, x => x == colName);
+                    try
+                    {
+                        row[idx] = token.Value.ToString().Replace("\r","<").Replace("\n", ">").Replace(",","");
+                    }
+                    catch
+                    {
+                        Console.WriteLine($"Cant Find Row {colName}");
+                    }
+                }
+            }
+            return row;
+        }
+
         public static void WriteConversion(string file, JArray json)
         {
             List<string> collumns = new List<string>();
@@ -17,21 +64,7 @@ namespace JSON_To_CSV
 
             foreach (JObject obj in json)
             {
-                List<string> row = new List<string>();
-                for (int i = 0; i < collumns.Count; i++) row.Add("");
-
-                foreach (JProperty token in obj.Children())
-                {
-                    if (!collumns.Contains(token.Name))
-                    {
-                        collumns.Add(token.Name);
-                        row.Add("");
-                    }
-
-                    int idx = collumns.FindIndex(0, x => x == token.Name);
-                    row[idx] = token.Value.ToString();
-                }
-                rows.Add(row);
+                rows.Add(GetRow(obj, ref collumns));
             }
 
             string col_names = String.Join(",", collumns);
